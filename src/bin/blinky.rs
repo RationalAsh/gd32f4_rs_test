03c7436824e52702d9e77348e217fffc7b494c32
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-09-09 13:27:52 +0800

    System Init function

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use gd32f4::gd32f425::{self as pac, Rcu};
+use gd32f4::gd32f425::{self as pac, Pmu, Rcu};
 // use gd32f4::gd32f425::rcu:
 use core::arch::asm;
 use libm::sinf;
@@ -49,14 +49,14 @@ fn main() -> ! {
         // Increment the counter
         ctr += 1;
         time = ctr as f32 * 0.01;
-        sin = sinf(time);
     }
 }
 
 fn system_init() {
-    use pac::rcu::*;
     // Create a handle to the RCU peripheral
     let rcu = unsafe { Rcu::steal() };
+    // Create a handle to the PMU peripheral
+    let pmu = unsafe { Pmu::steal() };
 
     // Set the IRC16MEN bit to enable the internal 16 MHz RC oscillator
     rcu.ctl0().modify(|_, w| w.irc16men().set_bit());
@@ -71,5 +71,107 @@ fn system_init() {
         }
     }
 
-    // Set the RCU_CFG0 register to select the internal 16 MHz RC oscillator as the system clock
+    rcu.cfg0()
+        .modify(|_, w| unsafe { w.ahbpsc().bits(9 as u8) });
+
+    // Spin for 50000 cycles
+    for _ in 0..50000 {
+        unsafe {
+            asm!("nop");
+        }
+    }
+
+    // Configure CFG0
+    // rcu.cfg0()
+    //     .modify(|_, w| unsafe{ w.scs(0b00 as u8) });
+    /* Reset HXTALEN, CKMEN and PLLEN bits */
+    rcu.ctl0().modify(|_, w| {
+        w.hxtalen()
+            .clear_bit()
+            .ckmen()
+            .clear_bit()
+            .pllen()
+            .clear_bit()
+    });
+
+    /* Reset HSEBYP bit */
+    rcu.ctl0().modify(|_, w| w.hxtalbps().clear_bit());
+
+    /* Reset CFG0 register */
+    rcu.cfg0().reset();
+
+    // Wait until IRC16M is selected as system clock
+    while rcu.cfg0().read().scs().bits() != 0b00 {}
+
+    /* Reset PLLCFGR register */
+    rcu.pll().modify(|_, w| unsafe { w.bits(0x24003010) });
+
+    /* Disable all interrupts */
+    // RCU_INT = 0x00000000U;
+    rcu.int().write(|w| unsafe { w.bits(0x00000000) });
+
+    // Configure RCU for 8MHz external clock and 200MHz clock speed
+    // Enable HXTAL
+    rcu.ctl0().modify(|_, w| w.hxtalen().set_bit());
+
+    /* wait until HXTAL is stable or the startup time is longer than HXTAL_STARTUP_TIMEOUT */
+    let mut timeout: u32 = 0;
+    while rcu.ctl0().read().hxtalstb().bit_is_clear() {
+        timeout += 1;
+        if timeout > 100000 {
+            break;
+        }
+    }
+
+    // If that failed
+    if timeout > 100000 {
+        panic!("HXTAL failed to start");
+    }
+
+    // ENABLE APB1 PMU
+    rcu.apb1en().modify(|_, w| w.pmuen().set_bit());
+    // Set PMU to LDO VS
+    pmu.ctl().modify(|_, w| unsafe { w.ldovs().bits(0b11) });
+
+    // Now the crystal is stable, we can use it as the system clock
+    // Set the AHB prescaler to 1
+    rcu.cfg0().modify(|_, w| unsafe { w.ahbpsc().bits(0b000) });
+    // Set APB2 to AHB/2
+    rcu.cfg0().modify(|_, w| unsafe { w.apb2psc().bits(0b100) });
+    // Set APB1 to AHB/4
+    rcu.cfg0().modify(|_, w| unsafe { w.apb1psc().bits(0b101) });
+
+    /* Configure the main PLL, PSC = 8, PLL_N = 400, PLL_P = 2, PLL_Q = 9 */
+    //    RCU_PLL = (8U | (400U << 6U) | (((2U >> 1U) - 1U) << 16U) |
+    //               (RCU_PLLSRC_HXTAL) | (9U << 24U));
+    rcu.pll().modify(|_, w| unsafe {
+        w.pllpsc()
+            .bits(8)
+            .plln()
+            .bits(400)
+            .pllp()
+            .bits(2)
+            .pllq()
+            .bits(9)
+            .pllsel()
+            .set_bit()
+    });
+
+    // Enable PLL
+    rcu.ctl0().modify(|_, w| w.pllen().set_bit());
+
+    // Wait until PLL is stable
+    while rcu.ctl0().read().pllstb().bit_is_clear() {}
+
+    // Enable the high-drive to extend to clock frequency to 200MHz
+    pmu.ctl().modify(|_, w| w.hden().set_bit());
+    // Wait until the high-drive is stable
+    while pmu.cs().read().hdrf().bit_is_clear() {}
+    // Select high-drive mode
+    pmu.ctl().modify(|_, w| w.hds().set_bit());
+    // Wait until the high-drive is stable
+    while pmu.cs().read().hdsrf().bit_is_clear() {}
+
+    // Wait until PLL is selected as the system clock
+    while rcu.cfg0().read().scss().bits() == 0b00 {}
 }
